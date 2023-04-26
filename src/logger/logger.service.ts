d1dd74d6586bf77304ce7a704ee09ef117699658
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-04-26 17:57:50 +0300

    feat: logger service

---
+++ src/logger/logger.service.ts
@@ -0,0 +1,25 @@
+import { Logger, ILogObj } from 'tslog';
+
+export class LoggerService {
+  public logger: Logger<ILogObj>;
+
+  constructor() {
+    this.logger = new Logger({
+      name: 'MainLogger',
+    });
+  }
+
+  log(...args: unknown[]) {
+    this.logger.info(...args);
+  }
+
+  error(...args: unknown[]) {
+    this.logger.error(...args);
+  }
+
+  warn(...args: unknown[]) {
+    this.logger.warn(...args);
+  }
+
+
+}
